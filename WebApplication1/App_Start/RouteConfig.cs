c491a8b6de74665835946200d107583a7dcee4b2
Pedro Cruz <pedrolucas11@live.com>
2020-03-28 23:52:16 -0300

    Teste de configuração das rotas.

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -17,6 +17,12 @@ namespace WebApplication1
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    "EditarWin",
+            //    "Editar/{id}",
+            //    new { controle = "Registro", Action = "Editar" }
+            //    );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
